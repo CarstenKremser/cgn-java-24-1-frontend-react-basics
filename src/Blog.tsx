b72c1557418d6f64000b20d8c52cd2bcd1e9d326
Carsten Kremser <github@carstenkremser.de>
2024-04-16 15:28:30 +0200

    Add empty Blog component

---
+++ src/Blog.tsx
@@ -0,0 +1,20 @@
+// Create a new React project with Vite and set up the basic structure.
+//
+// Step 1: Create a component called Blog and add it to App.tsx.
+//
+// Step 2: Take your blog from the HTML task and recreate it in React.
+// Divide your blog thoughtfully into components. The components should
+// come together in the Blog component.
+//
+// If you have already completed this task earlier, you may work on the
+// bonus task on the next page.
+
+
+function Blog():JSX.Element {
+    return  <>
+        <p>BLOG</p>
+    </>
+
+}
+
+export default Blog
